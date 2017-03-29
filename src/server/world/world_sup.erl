cbfbe1b97a691ecba2b8a466bad217c2a5ea167d
linanjing1105 <763456927@qq.com>
2017-03-29 12:01:47 +0800

    世界

--- src/server/world/world_sup.erl
+++ src/server/world/world_sup.erl
@@ -13,8 +13,8 @@ init([]) ->
 	SvrList = [	
 			   {login,		{world_svr,start_link,[login,fun_login]},permanent, 5000, worker, [world_svr]},
 			   %{mynet_mng,	{world_svr,start_link,[mynet_mng,fun_mynet_mng]},permanent, 5000, worker, [world_svr]},
-			  % {scene_mng, 	{world_svr,start_link,[scene_mng,fun_scene_mng]},permanent, 5000, worker, [world_svr]},
-%			   {agent_mng, 	{world_svr,start_link,[agent_mng,fun_agent_mng]},permanent, 5000, worker, [world_svr]},
+			   {scene_mng, 	{world_svr,start_link,[scene_mng,fun_scene_mng]},permanent, 5000, worker, [world_svr]},
+			   {agent_mng, 	{world_svr,start_link,[agent_mng,fun_agent_mng]},permanent, 5000, worker, [world_svr]}
 			  % {offline_agent_mng, 	{world_svr,start_link,[offline_agent_mng,fun_offline_agent_mng]},permanent, 5000, worker, [world_svr]}
 			  ],
     RestartStrategy = {one_for_one, 3, 10},
