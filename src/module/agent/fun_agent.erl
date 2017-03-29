58cf31bb3976c9f8f6c60c672d6619b3da8808f4
linanjing1105 <763456927@qq.com>
2017-03-29 11:44:11 +0800

    启动逻辑修改

--- src/module/agent/fun_agent.erl
+++ src/module/agent/fun_agent.erl
@@ -4,11 +4,10 @@
 -include("common.hrl").
 
 
--export([on_init/1,on_login/1,on_logout/0,do_time/1,do_msg/1,fresh_all_prop/1,task_count_for_active_spcieal/3,task_count_for_active_spcieal/2,
-		 refresh_num/1]).
+-export([on_init/1,do_time/1,do_msg/1,on_login/1]).
 
  
-on_init({Sid,Ip,Seq,Aid,Uid,AgentIdx}) ->
+on_init({Sid,Ip,_Seq,Aid,Uid,AgentIdx}) ->
 	?debug("on_init  ....Seq=~p",[Seq]),
 	{{_,_,Day},_}=util:unix_to_localtime(util:unixtime()),
 	put(sid,Sid),
@@ -22,83 +21,65 @@ on_init({Sid,Ip,Seq,Aid,Uid,AgentIdx}) ->
 	ok.
 
 
-on_login(Seq)->
-	Uid = get(uid),
-	case db:dirty_get(usr, Uid) of
-		[Usr] ->
-			Sid = get(sid),
-			Ip = get(ip),
-			if 
-				Usr#usr.last_login_time == 0 ->
-					%%新角色首次登录
-					on_first_login(Seq);
-				true -> skip
-			end,
-			Camp=1,
-			Hp = max(0, Usr#usr.hp),
-			Mp = max(0, Usr#usr.mp),	
-			NewUsr = Usr#usr{hp=Hp,mp=Mp,last_login_time=util:unixtime()},
-			db:dirty_put(NewUsr),		
-			%%战力初始化需要在其他系统之后		
-			Fight_score=0,
-			server_tools:send_to_agent_mng({agent_in,NewUsr,Sid,Ip,InitData,self(),get(agentIdx)}),
-			Pt = pt_usr_info_b102:new(),
-			Pt1 = Pt#pt_usr_info{
-								 id=Usr#usr.id,  
-								 name=util:to_list(Usr#usr.name),
-								 level=Usr#usr.lev,
-								 prof=Usr#usr.prof,
-								 cur_hp=Hp,  
-								 cur_mp=Mp,
-								 exp=Usr#usr.exp,   
-								 property_list=[],
-								 equip_id_list= [],
-								 camp=Camp,
-								 resource_list = [],
-								 model_clothes_id=[], 
-								 title_id=0,	
-								 guild_name=[],
-								 sla=Usr#usr.sla,	
-							 	 fiight_score=0
-								 },
-			?send(Sid,pt_usr_info_b102:to_binary(Pt1, Seq)),
-			sync_time(Sid);
-		_ ->
-			?log_warning("on_login failed,uid=~p", [Uid])
-	end,
-	ok.
+on_login(_Seq)->io:format("===========~p~n",[_Seq]).
+	% Uid = get(uid),
+	% case db:dirty_get(usr, Uid) of
+	% 	[Usr] ->
+	% 		Sid = get(sid),
+	% 		Ip = get(ip),
+	% 		if 
+	% 			Usr#usr.last_login_time == 0 ->
+	% 				%%新角色首次登录
+	% 				on_first_login(Seq);
+	% 			true -> skip
+	% 		end,
+	% 		% db:dirty_put(NewUsr),		
+	% 		%%战力初始化需要在其他系统之后		
+	% 		Fight_score=0;
+	% 		% server_tools:send_to_agent_mng({agent_in,NewUsr,Sid,Ip,InitData,self(),get(agentIdx)});
+	% 		% Pt = pt_usr_info_b102:new(),
+	% 		% Pt1 = Pt#pt_usr_info{
+	% 		% 					 id=Usr#usr.id,  
+	% 		% 					 name=util:to_list(Usr#usr.name),
+	% 		% 					 level=Usr#usr.lev,
+	% 		% 					 exp=Usr#usr.exp,   	
+	% 		% 				 	 fiight_score=0
+	% 		% 					 };
+	% 		% ?send(Sid,pt_usr_info_b102:to_binary(Pt1, Seq)),
+	% 		% sync_time(Sid);
+	% 	_ ->
+	% 		?log_warning("on_login failed,uid=~p", [Uid])
+	% end,
+	% ok.
 
-on_first_login(Seq) ->
-	Uid = get(uid),
-	Sid = get(sid),
-	ok.	 
-on_logout() ->
-	Uid = get(uid),
-	server_tools:send_to_agent_mng({agent_out, Uid}),
-	ok.
+% on_first_login(Seq) ->
+% 	% Uid = get(uid),
+% 	% Sid = get(sid),
+% 	ok.	 
+% on_logout() ->
+% 	% Uid = get(uid),
+% 	% server_tools:send_to_agent_mng({agent_out, Uid}),
+% 	ok.
 do_msg({process_pt, PtModule, Seq, Pt, Sid}) ->
 	process_pt(PtModule, Seq, Pt, Sid);		
-do_msg({start_fly, Sid,_Uid,Seq, SceneId,{Scene,Pos}}) ->
+% do_msg({start_fly, Sid,_Uid,Seq, SceneId,{Scene,Pos}}) ->
 	%服务端场景准备完毕，通知客户端加载场景
-	?send(Sid,pt_req_load_scene_b104:to_binary(Pt1, Seq)),
-	ok;
-do_msg({add_exp, Exp}) ->ok;
+	% ?send(Sid,pt_req_load_scene_b104:to_binary(Pt1, Seq)),
+	% ok;
+do_msg({add_exp, Exp}) ->Exp;
 do_msg(_Msg) ->
 	?log_warning("unknown msg,msg=~p",[_Msg]),
 	ok.
 
-do_time(Time) ->
+do_time(_Time) ->
 	ok.
 
-process_pt(pt_usr_enter_scene_b003,Seq,Pt,Sid) ->ok;
-	%% 登录游戏后请求场景
-%% 客户端场景加载完成时 %
-process_pt(pt_load_scene_finish_b005,Seq,Pt,Sid) ->ok;
+
 process_pt(_PtName,_Seq,_Pt,_Sid) ->
 	?log_warning("unprocessed proto:~p",[_PtName]),
 	ok.
 %% agent通用0点刷新定时器
-refresh_num(Uid) ->ok;
+% refresh_num(Uid) ->ok.
 	% case db:dirty_get(usr,Uid) of
 	% 	[Usr] -> 
 	% 		Taday_Time=util:get_relative_day(?COMMOM_REST_HOUR),  
--- src/module/login/fun_login.erl
+++ src/module/login/fun_login.erl
@@ -3,7 +3,7 @@
 -include("common.hrl").
 
 -export([do_init/0,do_close/0,do_call/1,do_info/1,do_time/1,do_msg/1]).
--export([lyn_auth_callback/2]).
+-export([]).
 
 -record(login_data, {sid,login_closed = false, aid=0,uid=0,ip=0,key=0,usrlist=[]}).
 
@@ -62,117 +62,104 @@ get_login_data(Sid) ->
 		[] -> null;
         Other -> ?log_error("Error:get login_data error,Sid=~p,Other=~p",[Sid,Other]),?UNDEFINED
     end.
-match_login_data(Pat) ->
-	ets:match_object(login_data, Pat).
+% match_login_data(Pat) ->
+% 	ets:match_object(login_data, Pat).
 put_login_data(Data) ->
 	ets:insert(login_data, Data).
 del_login_data(Key) ->
 	ets:delete(login_data, Key).
 
-is_valid_account(Account) ->
-	case length(Account) > 0 of
-		true ->
-			case re:run(Account, "[^a-zA-Z0-9_]") of
-				nomatch -> true;
-				_ -> false
-			end;
-		_ -> false
-	end.
+% is_valid_account(Account) ->
+% 	case length(Account) > 0 of
+% 		true ->
+% 			case re:run(Account, "[^a-zA-Z0-9_]") of
+% 				nomatch -> true;
+% 				_ -> false
+% 			end;
+% 		_ -> false
+% 	end.
 
-is_valid_name(Name) ->
-	if
-		Name == "" -> false;
-		true ->
-			case length(xmerl_ucs:from_utf8(Name)) > ?MAX_NAME_LEN of
-				true -> false;
-				_ ->
-					case fun_sensitive_words:check_sensitive_words(Name) of
-						true -> true;
-						_ -> {false, invalid_words}
-					end
-			end
-	end.
-
-process_pt(pt_req_create_usr_a006,Seq,Pt,Sid,_Ip) -> 
-	?debug("get req_create_usr Pt = ~p,Sid = ~p",[Pt,Sid]),
-	Name =Pt#pt_req_create_usr.name,
-	Prof =Pt#pt_req_create_usr.prof,
-	case is_valid_name(Name) of
-		true -> 
-			case find_usr_by_name(Name) of
-				[] -> create_usr(Name,Prof,Sid,Seq);
-				_ -> ?error_report(Sid,"usr_name_renam",Seq)
-			end;
-		{false, invalid_words} -> ?error_report(Sid,"pet10",Seq);
-		_ -> ?error_report(Sid,"check_data_error",Seq)
-	end,
-	ok;
+% is_valid_name(Name) ->
+% 	if
+% 		Name == "" -> false;
+% 		true ->
+% 			case length(xmerl_ucs:from_utf8(Name)) > 10 of
+% 				true -> false;
+% 				_ ->
+% 					case fun_sensitive_words:check_sensitive_words(Name) of
+% 						true -> true;
+% 						_ -> {false, invalid_words}
+% 					end
+% 			end
+% 	end.
 
 
-process_pt(pt_login_a001,Seq,Pt,Sid,Ip) ->
-	?debug("get login Pt = ~p,Sid = ~p",[Pt,Sid]),
-	Account = Pt#pt_login.account, 
-	Password = Pt#pt_login.password,
-	Platform = Pt#pt_login.platform,
+
+
+% process_pt(pt_login_a001,Seq,Pt,Sid,Ip) ->
+% 	?debug("get login Pt = ~p,Sid = ~p",[Pt,Sid]),
+% 	Account = Pt#pt_login.account, 
+% 	Password = Pt#pt_login.password,
+% 	Platform = Pt#pt_login.platform,
 	
-	case get_login_data(Sid) of
-		#login_data{} -> 
-			?error_report(Sid,"already_login",Seq),
-			?discon(Sid, login_failed, 100);
-		_ ->
-			case Platform of
-				?PLATFORM_NULL ->
-					do_real_login(Sid, Account, Password, Platform, Seq, Ip);
-				?PLATFORM_LYNSDK ->
-					lyn_auth(Sid, Seq, Account, Platform, Ip);
-				_ ->
-					?error_report(Sid,"check_data_error",Seq),
-					?log_warning("unknown login platform,platform=~p", [Platform]),
-					?discon(Sid, login_failed, 100)
-			end
-	end;
+% 	case get_login_data(Sid) of
+% 		#login_data{} -> 
+% 			% ?error_report(Sid,"already_login",Seq),
+% 			?discon(Sid, login_failed, 100);
+% 		_ ->
+% 			case Platform of
+% 				?PLATFORM_NULL ->
+% 					do_real_login(Sid, Account, Password, Platform, Seq, Ip);
+% 				?PLATFORM_LYNSDK ->
+% 					lyn_auth(Sid, Seq, Account, Platform, Ip);
+% 				_ ->
+% 					?error_report(Sid,"check_data_error",Seq),
+% 					?log_warning("unknown login platform,platform=~p", [Platform]),
+% 					?discon(Sid, login_failed, 100)
+% 			end
+% 	end;
 
-process_pt(pt_usr_enter_b001,Seq,Pt,Sid,Ip) ->
-	Uid = Pt#pt_usr_enter.uid,
-	Key = Pt#pt_usr_enter.key,
-	?debug("pt_usr_enter_b001............................."),
-	%%TODO:verify key
-	case match_login_data(#login_data{uid = Uid, key=Key, ip=Ip, _ = '_'}) of
-		[#login_data{aid=Aid, sid = OldSid} | _] ->	
-			del_login_data(OldSid),
-			%%开始进入游戏
-			server_tools:send_to_agent_mng({let_it_in, Sid,Ip,Seq,Aid,Uid}),
-			ok;
-		_ ->
-			?discon(Sid, login_failed, 100)
-	end;
+% process_pt(pt_usr_enter_b001,Seq,Pt,Sid,Ip) ->
+% 	Uid = Pt#pt_usr_enter.uid,
+% 	Key = Pt#pt_usr_enter.key,
+% 	?debug("pt_usr_enter_b001............................."),
+% 	%%TODO:verify key
+% 	case match_login_data(#login_data{uid = Uid, key=Key, ip=Ip, _ = '_'}) of
+% 		[#login_data{aid=Aid, sid = OldSid} | _] ->	
+% 			del_login_data(OldSid),
+% 			%%开始进入游戏
+% 			server_tools:send_to_agent_mng({let_it_in, Sid,Ip,Seq,Aid,Uid}),
+% 			ok;
+% 		_ ->
+% 			?discon(Sid, login_failed, 100)
+% 	end;
 	
 process_pt(Name,_Seq,_Pt,_Sid,_Ip) ->
 	?log_warning("unprocessed proto:~p",[Name]).
 
 
-load_account(Account,Password,Platform) ->
-	BinAcc = util:to_binary(Account),
-	BinPwd = util:to_binary(Password),
-	AccList =
-	case db:dirty_get(account, BinAcc, #account.username) of
-		[] -> db:getnet(account, username, BinAcc);
-		Other -> Other
-	end,
-	case AccList of
-		[#account{id = Aid, password = BinPwd, platform=Platform}] -> {ok, Aid};
-		[] ->
-			case db:insert(#account{username=BinAcc,password=BinPwd, platform=Platform}) of
-				[#account{id = Aid}] ->
-					?log_trace("no acc,create one,acc = ~p pass = ~p",[Account,Password]),
-					{ok, Aid};
-				_ ->
-					?log_warning("create acc error,acc=~p pass=~p", [Account,Password]),
-					{false, "check_data_error"}
-			end;
-		_ ->
-			{false, "password_error"}
-	end.
+% load_account(Account,Password,Platform) ->
+% 	BinAcc = util:to_binary(Account),
+% 	BinPwd = util:to_binary(Password),
+% 	AccList =
+% 	case db:dirty_get(account, BinAcc, #account.username) of
+% 		[] -> db:getnet(account, username, BinAcc);
+% 		Other -> Other
+% 	end,
+% 	case AccList of
+% 		[#account{id = Aid, password = BinPwd, platform=Platform}] -> {ok, Aid};
+% 		[] ->
+% 			case db:insert(#account{username=BinAcc,password=BinPwd, platform=Platform}) of
+% 				[#account{id = Aid}] ->
+% 					?log_trace("no acc,create one,acc = ~p pass = ~p",[Account,Password]),
+% 					{ok, Aid};
+% 				_ ->
+% 					?log_warning("create acc error,acc=~p pass=~p", [Account,Password]),
+% 					{false, "check_data_error"}
+% 			end;
+% 		_ ->
+% 			{false, "password_error"}
+% 	end.
 
 
 
@@ -180,58 +167,58 @@ load_account(Account,Password,Platform) ->
 
 
 
-sign(Uid, TimeStamp, RandStr) ->
-	util:md5(io_lib:format("~p#~p#~s#~s", [Uid,TimeStamp,RandStr,?KEY])).
+% sign(Uid, TimeStamp, RandStr) ->
+% 	util:md5(io_lib:format("~p#~p#~s#~s", [Uid,TimeStamp,RandStr,?KEY])).
 
-gen_key(Uid) ->
-	TimeStamp = util:unixtime(),
-	RandStr = util:random_string(16),
-	sign(Uid, TimeStamp, RandStr).
+% gen_key(Uid) ->
+% 	TimeStamp = util:unixtime(),
+% 	RandStr = util:random_string(16),
+% 	sign(Uid, TimeStamp, RandStr).
 	
 
 
 	
 
 
-real_login(Sid, StrAcc, StrPsw, Platform, Seq, Ip) ->
-	case get_login_data(Sid) of
-		#login_data{sid=Sid}->
-			?debug("already login"),
-			skip;
-		_ ->
-			do_real_login(Sid, StrAcc, StrPsw, Platform, Seq, Ip)
-	end.
+% real_login(Sid, StrAcc, StrPsw, Platform, Seq, Ip) ->
+% 	case get_login_data(Sid) of
+% 		#login_data{sid=Sid}->
+% 			?debug("already login"),
+% 			skip;
+% 		_ ->
+% 			do_real_login(Sid, StrAcc, StrPsw, Platform, Seq, Ip)
+% 	end.
 
-do_real_login(Sid, Account, Password, Platform, Seq, Ip) ->
-	?debug("do_real_login"),
-	case is_valid_account(Account) of
-		true ->
-			case load_account(Account, Password, Platform) of
-				{ok, Aid} ->
-					case match_login_data(#login_data{aid=Aid, _ = '_'}) of
-						[#login_data{sid = OldSid}] ->
-							del_login_data(OldSid),
-							?log_trace("relogin, account=~p", [Account]),
-							?error_report(OldSid,"relogin"),
-							?discon(OldSid, relogin, 100);
-						_ ->
-							case db:dirty_get(ply, Aid, #ply.aid) of
-								[#ply{sid = OldSid, uid=OldUid}] ->
-									?error_report(OldSid,"relogin"),
-									?log_trace("relogin, account=~p,oldUid=~p", [Account, OldUid]),
-									?discon(OldSid, relogin, 100);							
-								_ -> skip
-							end
-					end,
-					request_usr_list(Sid, Aid,Seq,Ip);
-				{false, Error} ->
-					?error_report(Sid, Error, Seq),
-					?discon(Sid, login_failed, 100)
-			end;
-		_ -> 
-			?error_report(Sid,"account_field_error",Seq),
-			?discon(Sid, login_failed, 100)
-	end.
+% do_real_login(Sid, Account, Password, Platform, Seq, Ip) ->
+% 	?debug("do_real_login"),
+% 	case is_valid_account(Account) of
+% 		true ->
+% 			case load_account(Account, Password, Platform) of
+% 				{ok, Aid} ->
+% 					case match_login_data(#login_data{aid=Aid, _ = '_'}) of
+% 						[#login_data{sid = OldSid}] ->
+% 							del_login_data(OldSid),
+% 							?log_trace("relogin, account=~p", [Account]);
+% 							% ?error_report(OldSid,"relogin"),
+% 							% ?discon(OldSid, relogin, 100);
+% 						_ ->
+% 							case db:dirty_get(ply, Aid, #ply.aid) of
+% 								[#ply{sid = OldSid, uid=OldUid}] ->
+% 									% ?error_report(OldSid,"relogin"),
+% 									?log_trace("relogin, account=~p,oldUid=~p", [Account, OldUid]);
+% 									% ?discon(OldSid, relogin, 100);							
+% 								_ -> skip
+% 							end
+% 					end,
+% 					request_usr_list(Sid, Aid,Seq,Ip);
+% 				{false, Error} ->
+% 					?error_report(Sid, Error, Seq),
+% 					?discon(Sid, login_failed, 100)
+% 			end;
+% 		_ -> 
+% 			?error_report(Sid,"account_field_error",Seq),
+% 			?discon(Sid, login_failed, 100)
+% 	end.
 
 
 						
